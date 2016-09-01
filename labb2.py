872eb324def6c1563a4b77c42047b4ac41fb998c
97marcar <hans.hansson22@gmail.com>
2016-09-01 16:49:06 +0200

    aaa

--- labb2.py
+++ labb2.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-def bounce(n,n2):
-    if n >= 0:
+
+def bounce(n):
+    if n > 0:
         print(n),
-        bounce(n-1,n2)
-    elif n < 0 and n >= (n2*-1):
-        print(n*-1),
-        bounce(n-1,n2)
+        bounce(n-1)
+        print(n),
+    elif n == 0:
+        print(0),
     else:
         pass
 
-bounce(7,7)
+bounce(7)
