210de8d2120061e647348e798c9d41b37a5b12d9
97marcar <marcus.carlsson97@gmail.com>
2016-09-27 13:26:24 +0100

    ksksksksksksksksksksksksk

--- labb3.py
+++ labb3.py
@@ -1,18 +1,17 @@
 sla_upp = ["123","bok","stor"]
 beskrivning = ["123","sak","liten"]
 
-def ordlista(sla_upp, beskrivning):
+def ordlistaLIST(sla_upp, beskrivning):
     print("1: Insert")
     print("2: Lookup")
     print("3: Exit program")
     choice = int(input("Choose alternative: "))
     if choice == 1:
         word_to_insert = raw_input("Word to insert: ")
-        print(word_to_insert)
         sla_upp.append(word_to_insert)
         description_of_word = raw_input("Description of word: ")
         beskrivning.append(description_of_word)
-        ordlista(sla_upp,beskrivning)
+        ordlistaLIST(sla_upp,beskrivning)
     elif  choice == 2:
         index = ""
         word_to_lookup = raw_input("Word to lookup: ")
@@ -21,14 +20,45 @@ def ordlista(sla_upp, beskrivning):
                 if word_to_lookup == sla_upp[n]:
                     index = n
                     print("Description for "+sla_upp[index]+ ": "+ beskrivning[index])
-                    ordlista(sla_upp,beskrivning)
+                    ordlistaLIST(sla_upp,beskrivning)
         else:
             print("That item is not in the dictionary")
-            ordlista(sla_upp,beskrivning)
+            ordlistaLIST(sla_upp,beskrivning)
 
     elif choice == 3:
         return
     else:
         print("Please choose option 1,2 or 3.")
 
-ordlista(sla_upp,beskrivning)
+tuplelist = []
+def ordlistaTUPLE(tuplelist):
+    print("1: Insert")
+    print("2: Lookup")
+    print("3: Exit program")
+    choice = int(input("Choose alternative: "))
+    if choice == 1:
+        word_to_insert = raw_input("Word to insert: ")
+        description_of_word = raw_input("Description of word: ")
+        tuplelist.append((word_to_insert,description_of_word))
+        ordlistaTUPLE(tuplelist)
+    elif  choice == 2:
+        index = ""
+        word_to_lookup = raw_input("Word to lookup: ")
+        word_found = False
+        for n in range(len(tuplelist)):
+            if word_to_lookup == tuplelist[n][0]:
+                word_found = True
+                print("Description for "+tuplelist[n][0]+ ": "+ tuplelist[n][1])
+                ordlistaTUPLE(tuplelist)
+        if word_found == False:
+                print("That item is not in the dictionary")
+                ordlistaLIST(sla_upp,beskrivning)
+    elif choice == 3:
+        return
+    else:
+        print("Please choose option 1,2 or 3.")
+        ordlistaTUPLE(tuplelist)
+
+#ordlistaTUPLE(tuplelist)
+
+ordlistaLIST(sla_upp,beskrivning)
---
+++ test.py
@@ -0,0 +1,19 @@
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
+
+print(states.keys()[0])
+print(states.keys()[1])
+print(states.keys()[2])
+print(states.keys()[3])
+print(states.keys()[4])
+
+print(states.values()[0])
+print(states.values()[1])
+print(states.values()[2])
+print(states.values()[3])
+print(states.values()[4])
